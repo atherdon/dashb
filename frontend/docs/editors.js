bdc3a95f54590a67aa25c3b4f7875f244da8c977
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-01-17 16:53:48 +0200

    Create editors.js

---
+++ frontend/docs/editors.js
@@ -0,0 +1,128 @@
+[
+ {
+   "FIELD1": "Editors",
+   "FIELD2": "Edited",
+   "FIELD3": "Edited, %",
+   "FIELD4": "Published",
+   "FIELD5": "Published, %",
+   "FIELD6": "Rejected",
+   "FIELD7": "Rejected, %"
+ },
+ {
+   "FIELD1": "Amy Tom",
+   "FIELD2": "27",
+   "FIELD3": "42.11%",
+   "FIELD4": "10",
+   "FIELD5": "-56.52%",
+   "FIELD6": "5",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Hackerhodl",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Natasha",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "David",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Kien",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Linh",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Arthur",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Storm",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Austin",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Dane",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Richard",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Limarc",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ },
+ {
+   "FIELD1": "Rianke",
+   "FIELD2": "10",
+   "FIELD3": "42.86%",
+   "FIELD4": "8",
+   "FIELD5": "0.00%",
+   "FIELD6": "10",
+   "FIELD7": "x"
+ }
+]
