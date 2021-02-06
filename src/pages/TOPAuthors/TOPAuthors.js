804edbabad875572c3b79d919fcb4b7ccb8965f2
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-02-06 11:50:48 +0400

    new links collection

--- src/pages/TOPAuthors/TOPAuthors.js
+++ src/pages/TOPAuthors/TOPAuthors.js
@@ -6,15 +6,15 @@ import { topAuthorLinks } from "data/TopAuthorLinks";
 import { columns } from './tableColumns';
 
 const TOPAutors = () => {
-    const dataSource = topAutors.map((item, index) => {
-        return {
-            key: index,
-            ...item,
-            'Draft url': topAuthorLinks[index]
-        };
-    });
-    console.log(dataSource)
-    return <Table dataSource={dataSource} columns={columns} />;
+  const dataSource = topAutors.map((item, index) => {
+    return {
+      key: index,
+      ...item,
+      'Draft url': topAuthorLinks[index]
+    };
+  });
+  console.log(dataSource)
+  return <Table dataSource={dataSource} columns={columns} />;
 };
 
 export default TOPAutors;
